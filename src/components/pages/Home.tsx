326842a6aaff404b33d5bf4042e0d4c67fac93d1
esenturDev <nurlanuuluesentur@gmail.com>
2024-03-30 10:57:51 +0600

    flexboxfroggy Project ended

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { HomePages } from "./home/HomePages";
 
 const Home = () => {
--- src/components/pages/home/HomePages.tsx
+++ src/components/pages/home/HomePages.tsx
@@ -1,12 +1,35 @@
-import { useEffect, useState } from "react";
+import {  useEffect, useState } from "react";
 import icon from "../../../assets/skip-left-fill.svg";
 import icon2 from "../../../assets/skip-right-fill.svg";
 import scss from "./HomePages.module.scss";
 import { Input } from "../../Ul/input/Input";
-import Button from "../../Ul/button/Button";
+// import Button from "../../Ul/button/Button";
+
+interface TypesLevelArray {
+	id: number;
+	name: string;
+	text: string;
+	nameText: string;
+	result1: string;
+	result2: string;
+	result3: string;
+	img1: string;
+	icon1: string;
+	isResult: boolean,
+	img2: string,
+	img3: string,
+	img4: string,
+	img5: string,
+	icon2: string,
+	icon3: string,
+	icon4: string,
+	icon5: string,
+	result11: string,
+	result22: string,
+}
 
 export const HomePages = () => {
-	const LevelArray = [
+	const LevelArray: TypesLevelArray[][] = [
 		[
 			{
 				id: 1,
@@ -21,6 +44,17 @@ export const HomePages = () => {
 				icon1:
 					"https://st3.depositphotos.com/4124697/14252/i/450/depositphotos_142522847-stock-photo-abstract-composition-color.jpg",
 				isResult: false,
+				img2: "",
+				img3: "",
+				img4: "",
+				img5: "",
+				icon2: "",
+				icon3: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
+
 			},
 		],
 
@@ -29,6 +63,7 @@ export const HomePages = () => {
 				id: 2,
 				name: "Используй justify-content ещё раз, чтобы помочь этим лягушатам попасть на их лилии. Помни, что это свойство CSS выравнивает элементы горизонтально и принимает следующие значения:",
 				text: "flex-start: элементы выравниваются по левой стороне контейнера. flex-end: элементы выравниваются по правой стороне контейнера. center: элементы выравниваются по центру контейнера. space-between: элементы отображаются с одинаковыми отступами между ними. space-around: элементы отображаются с одинаковыми отступами вокруг них.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -39,6 +74,14 @@ export const HomePages = () => {
 				icon2:
 					"https://upload.wikimedia.org/wikipedia/commons/d/d0/Color-yellow.JPG",
 				isResult: false,
+				img3: "",
+				img4: "",
+				img5: "",
+				icon3: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 
@@ -47,6 +90,7 @@ export const HomePages = () => {
 				id: 3,
 				name: "Помоги всем трём лягушатам найти их лилии, просто используя justify-content. В этот раз у лилий много пространства вокруг.",
 				text: "Если ты чувствуешь, что забыл возможные значения свойства, ты можешь навести курсор на название свойства, чтобы посмотреть их. Попробуй навести курсор на justify-content.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -60,12 +104,20 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
 			{
 				id: 4,
 				name: "Теперь лилии по краям уплыли к берегам, увеличив пространство между ними. Используй justify-content. В этот раз у лилий одинаковое расстояние между ними.",
+				text: "",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -79,6 +131,12 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
@@ -86,6 +144,7 @@ export const HomePages = () => {
 				id: 5,
 				name: "Теперь используй align-items, чтобы помочь лягушатам добраться до нижней части пруда. Это свойство CSS выравнивает элементы вертикально и принимает следующие значения:",
 				text: "flex-start: элементы выравниваются по верхнему краю контейнера. flex-end: элементы выравниваются по нижнему краю контейнера. center: элементы выравниваются вертикально по центру контейнера. baseline: элементы отображаются на базовой линии контейнера. stretch: элементы растягиваются, чтобы заполнить контейнер.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -99,6 +158,12 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
@@ -106,6 +171,7 @@ export const HomePages = () => {
 				id: 6,
 				name: "Направь лягушонка в центр пруда, используя justify-content и align-items вместе.",
 				text: "flex-start: элементы выравниваются по верхнему краю контейнера. flex-end: элементы выравниваются по нижнему краю контейнера. center: элементы выравниваются вертикально по центру контейнера. baseline: элементы отображаются на базовой линии контейнера. stretch: элементы растягиваются, чтобы заполнить контейнер.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -113,6 +179,16 @@ export const HomePages = () => {
 				icon1:
 					"https://colourlex.com/wp-content/uploads/2021/02/Emerald-green-painted-swatch.jpg",
 				isResult: false,
+				img2: "",
+				img3: "",
+				img4: "",
+				img5: "",
+				icon2: "",
+				icon3: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
@@ -120,6 +196,7 @@ export const HomePages = () => {
 				id: 7,
 				name: "Лягушатам снова нужно пересечь пруд. В этот раз к лилиям, с достаточно большим пространством вокруг них. Используй комбинацию justify-content и align-items.",
 				text: "flex-start: элементы выравниваются по верхнему краю контейнера. flex-end: элементы выравниваются по нижнему краю контейнера. center: элементы выравниваются вертикально по центру контейнера. baseline: элементы отображаются на базовой линии контейнера. stretch: элементы растягиваются, чтобы заполнить контейнер.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -133,6 +210,12 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
@@ -140,6 +223,7 @@ export const HomePages = () => {
 				id: 8,
 				name: "Лягушатам нужно выстроиться в том же порядке, что и лилии, используя flex-direction. Это свойство CSS задает направление, в котором будут расположены элементы в контейнере, и принимает следующие значения:",
 				text: "row: элементы размещаются по направлению текста. row-reverse: элементы отображаются в обратном порядке к направлению текста. column: элементы располагаются сверху вниз. column-reverse: элементы располагаются снизу вверх.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -153,6 +237,12 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
@@ -160,6 +250,7 @@ export const HomePages = () => {
 				id: 9,
 				name: "Помоги лягушатам расположиться на своих лилиях, используя flex-direction. Это свойство CSS задает направление, в котором располагаются элементы в контейнере, и принимает следующие значения:",
 				text: "row: элементы размещаются по направлению текста. row-reverse: элементы отображаются в обратном порядке к направлению текста. column: элементы распологаются сверху вниз. column-reverse: элементы распологаются снизу вверх.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -173,6 +264,12 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
@@ -180,6 +277,7 @@ export const HomePages = () => {
 				id: 10,
 				name: "Помоги лягушатам попасть на свои лилии. Хоть и кажется, что они почти на своих местах, всё же придётся применить и flex-direction, и justify-content, чтобы поместить их на свои лилии.",
 				text: "Заметь, что когда ты устанавливаешь направление в обратном порядке для ряда или колонки, начало (start) и конец (end) тоже меняются местами.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -193,6 +291,12 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
@@ -200,6 +304,7 @@ export const HomePages = () => {
 				id: 11,
 				name: "Помоги лягушатам найти их лилии с помощью flex-direction и justify-content.",
 				text: "Заметь, когда в качестве направления выбрана колонка, то justify-content влияет на вертикальное выравнивание, а align-items — на горизонтальное.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -213,12 +318,20 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
 			{
 				id: 12,
 				name: "Помоги лягушатам найти их лилии с помощью flex-direction и justify-content.",
+				text: "",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -232,12 +345,20 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
 			{
 				id: 13,
 				name: "Помоги лягушатам найти их лилии с помощью flex-direction, justify-content и align-items.",
+				text: "",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -251,6 +372,12 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
+				result11: "",
+				result22: "",
 			},
 		],
 		[
@@ -258,6 +385,7 @@ export const HomePages = () => {
 				id: 14,
 				name: "Иногда изменения порядка отображения элементов в контейнере недостаточно. В таких случаях мы можем применить свойство order для конкретных элементов. По умолчанию, значение этого свойства у элементов равно 0, но мы можем задать положительное или отрицательное целое число этому свойству.",
 				text: "Используй свойство order, чтобы разместить лягушат на своих лилиях.",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -273,12 +401,18 @@ export const HomePages = () => {
 				icon3:
 					"https://htmlcolorcodes.com/assets/images/colors/red-color-solid-background-1920x1080.png",
 				isResult: false,
+				img4: "",
+				img5: "",
+				icon4: "",
+				icon5: "",
 			},
 		],
 		[
 			{
 				id: 15,
 				name: "Используй свойство order, чтобы отправить красного лягушонка на его лилию.",
+				text: "",
+				nameText: "",
 				result1: "#pond {",
 				result2: "display: flex;",
 				result3: "}",
@@ -315,6 +449,8 @@ export const HomePages = () => {
 	const [openDiv, setOpenDiv] = useState<boolean>(false);
 	const result = LevelArray.slice(0, 8);
 	console.log(result);
+	console.log(result4);
+	
 
 	const resultIndex = result[0][0];
 	const resultIndex2 = result[1][0];
@@ -346,33 +482,6 @@ export const HomePages = () => {
 			setResult3(false);
 		}
 	}, [count]);
-
-	// function buttonStyle() {
-
-	// 	resultIndex.isResult === true;
-	// 	// if (inputValueResult1 === "flex-end") {
-	// 	// 	resultIndex.isResult === true;
-	// 	// } else {
-	// 	// 	resultIndex.isResult === false;
-	// 	// }
-	// }
-
-	// useEffect(() => {
-	// 	if (inputValueResult1 === "flex-end") {
-	// 		buttonStyleResult === true;
-	// 	} else {
-	// 		buttonStyleResult === false;
-	// 	}
-	// }, [inputValueResult1]);
-
-	// useEffect(() => {
-	// 	if(buttonStyleResult === true) {
-	// 		resultIndex.isResult === true;
-	// 	} else if(buttonStyleResult === false) {
-	// 		resultIndex.isResult === false;
-	// 	}
-	// } , [buttonStyleResult])
-
 	if (inputValueResult1 === "flex-end") {
 		buttonStyleResult === true;
 	} else {
@@ -609,10 +718,10 @@ export const HomePages = () => {
 										<div className={scss.divForm}>
 											<p className={scss.textIsInputLabel}>
 												justify-content{" "}
-												<input
+												<Input
 													value={inputValueResult1}
 													type="text"
-													onChange={(e) => setInputValueResult1(e.target.value)}
+													setData={setInputValueResult1}
 												/>
 											</p>
 										</div>
@@ -632,13 +741,13 @@ export const HomePages = () => {
 																backgroundColor: "red",
 																paddingInline: "1rem",
 																paddingBlock: "8px",
-														  }
+														}
 														: {
 																backgroundColor: "rgba(255, 0, 0, 0.588)",
 																paddingInline: "0.9rem",
 																paddingBlock: "7px",
 																border: "nome",
-														  }
+														}
 													// {border: "no"}
 												}>
 												Далее
@@ -681,13 +790,13 @@ export const HomePages = () => {
 																backgroundColor: "red",
 																paddingInline: "1rem",
 																paddingBlock: "8px",
-														  }
+														}
 														: {
 																backgroundColor: "rgba(255, 0, 0, 0.588)",
 																paddingInline: "0.9rem",
 																paddingBlock: "7px",
 																border: "nome",
-														  }
+														}
 													// {border: "no"}
 												}>
 												Далее
@@ -731,13 +840,13 @@ export const HomePages = () => {
 																backgroundColor: "red",
 																paddingInline: "1rem",
 																paddingBlock: "8px",
-														  }
+														}
 														: {
 																backgroundColor: "rgba(255, 0, 0, 0.588)",
 																paddingInline: "0.9rem",
 																paddingBlock: "7px",
 																border: "nome",
-														  }
+														}
 													// {border: "no"}
 												}>
 												Далее
